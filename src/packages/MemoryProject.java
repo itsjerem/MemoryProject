0036f6f1792e28322eb456f59529dc4b89e50eb8
itsjerem <jeremytroccaz@gmail.com>
2024-02-13 15:39:55 +0100

    Ajouter des options et un minuteur à l'interface utilisateur

--- src/packages/MemoryProject.java
+++ src/packages/MemoryProject.java
@@ -3,6 +3,12 @@ package packages;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Color;
 
 public class MemoryProject {
 
@@ -38,6 +44,22 @@ public class MemoryProject {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		frame.getContentPane().add(panel, BorderLayout.NORTH);
+		panel.setLayout(new GridLayout(2, 0, 0, 0));
+		
+		JLabel lblSettings = new JLabel("Options");
+		lblSettings.setBackground(Color.WHITE);
+		lblSettings.setToolTipText("Options");
+		panel.add(lblSettings);
+		
+		JLabel lblTimer = new JLabel("Temps :");
+		lblTimer.setBackground(Color.GRAY);
+		lblTimer.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTimer.setToolTipText("temps");
+		panel.add(lblTimer);
 	}
 
 }
